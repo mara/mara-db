7dd723c0ae2d5bf369f201c265a0e18c49136cf7
jacopofar <jacopofar@users.noreply.github.com>
2017-09-07 17:13:24 +0200

    Assign each schema always the same color

--- mara_db/views.py
+++ mara_db/views.py
@@ -1,6 +1,9 @@
 """DB schema visualization"""
 
+import binascii
+import hashlib
 from typing import List, NamedTuple, Optional, Tuple
+from functools import lru_cache
 
 import flask
 import graphviz
@@ -17,6 +20,23 @@ mara_db = flask.Blueprint('mara_db', __name__, static_folder='static', url_prefi
 
 acl_resource = acl.AclResource(name='DB')
 
+schema_colors = ['#8B0000','#A52A2A','#B22222','#DC143C','#FF0000','#FF6347','#FF7F50','#CD5C5C','#F08080','#E9967A','#FA8072','#FFA07A','#FF4500','#FF8C00','#FFA500','#FFD700','#B8860B','#DAA520','#EEE8AA','#BDB76B','#F0E68C','#808000','#FFFF00','#9ACD32','#556B2F','#6B8E23','#7CFC00','#7FFF00','#ADFF2F','#006400','#008000','#228B22','#00FF00','#32CD32','#90EE90','#98FB98','#8FBC8F','#00FA9A','#00FF7F','#2E8B57','#66CDAA','#3CB371','#20B2AA','#2F4F4F','#008080','#008B8B','#00FFFF','#00FFFF','#E0FFFF','#00CED1','#40E0D0','#48D1CC','#AFEEEE','#7FFFD4','#B0E0E6','#5F9EA0','#4682B4','#6495ED','#00BFFF','#1E90FF','#ADD8E6','#87CEEB','#87CEFA','#191970','#000080','#00008B','#0000CD','#0000FF','#4169E1','#8A2BE2','#4B0082','#483D8B','#6A5ACD','#7B68EE','#9370DB','#8B008B','#9400D3','#9932CC','#BA55D3','#800080','#D8BFD8','#DDA0DD','#EE82EE','#FF00FF','#DA70D6','#C71585','#DB7093','#FF1493','#FF69B4','#FFB6C1','#FFC0CB','#FAEBD7','#F5F5DC','#FFE4C4','#FFEBCD','#F5DEB3','#FFF8DC','#FFFACD','#FAFAD2','#FFFFE0','#8B4513','#A0522D','#D2691E','#CD853F','#F4A460','#DEB887','#D2B48C','#BC8F8F','#FFE4B5','#FFDEAD']
+
+
+@lru_cache(None)
+def schema_color(name: str):
+    """Give a string a color, always the same for the same string.
+    Args:
+       name (:obj:`str`): A string
+
+    Returns:
+       str: An RGB color in hex format, always the same for the same string
+    """
+    # python hash function is not guaranteed to give the same result across versions or implementations
+    m = hashlib.sha256()
+    m.update(name.encode())
+    hash_index = int(binascii.hexlify(m.digest()), 16)
+    return schema_colors[hash_index % len(schema_colors)]
 
 def navigation_entry():
     return navigation.NavigationEntry(
@@ -63,7 +83,7 @@ def draw_schema(db: engine.Engine, schemas: List[str] = []) -> str:
         separator = '  '
         graph.node(shape='plain', name=f'{schema_name}__{table_name}',
                    label=''.join(['<',
-                                  """ <TABLE BORDER="1" CELLBORDER="0" CELLSPACING="0" CELLPADDING="1" BGCOLOR="#bbffcc"><TR><TD ALIGN="LEFT"><U><B>""",
+                                  f""" <TABLE BORDER="1" CELLBORDER="0" CELLSPACING="0" CELLPADDING="1" BGCOLOR="{schema_color(schema_name)}"><TR><TD ALIGN="LEFT"><U><B>""",
                                   f'{schema_name}.{table_name}' if len(schemas) > 1 else table_name,
                                   """</B></U></TD></TR>""", ''.join(
                            [f'<TR><TD ALIGN="LEFT" >{separator}{c}{separator}</TD></TR>' for c in columns]),
