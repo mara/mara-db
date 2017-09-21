855bdc4cf76ecef4d208dfbc868eca2c8b6ec4a3
jacopofar <jacopofar@users.noreply.github.com>
2017-09-21 19:02:48 +0200

    Bump to version 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mara-db',
-    version='1.0.1',
+    version='1.1.0',
 
     description='Configuration and monitoring of database connections',
 
