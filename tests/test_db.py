b22642a6fae1da46f83f65288eaf166e4afe1c85
jacopofar <jacopofar@users.noreply.github.com>
2017-11-02 08:53:02 +0100

    Manage the sqlite main schema properly without workarounds

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,10 @@
 import json
+import os
 import pytest
 import pathlib
 import sqlite3
 import tempfile
+import random
 import sys
 
 from sqlalchemy import engine
@@ -12,14 +14,11 @@ from mara_app import monkey_patch
 import mara_db
 
 
-
-
-
-
 class TestApp:
+
     @pytest.fixture(autouse=True)
     def set_up(self):
-        test_target = str(pathlib.Path(tempfile.mkdtemp()) / 'test_mara_db.db')
+        test_target = str(pathlib.Path(tempfile.mkdtemp()) / f'test_mara_db_{str(random.randint(1, 99999))}.db')
         conn = sqlite3.connect(test_target)
         conn.execute('CREATE TABLE table_one(column_one INTEGER, column_two TEXT)')
         conn.execute("""CREATE TABLE table_two(
@@ -30,9 +29,12 @@ class TestApp:
         conn.close()
         @monkey_patch.patch(mara_db.config.databases, 'Replace the default database with a mockup')
         def wrapper():
-            return {'test_db': engine.create_engine(f'sqlite+pysqlite:////{test_target}')}
+            return {'test_db': engine.create_engine(f'sqlite:////{test_target}')}
         app = MaraApp()
         self.app = app.test_client()
+        # teardown, remove the database
+        yield None
+        os.unlink(test_target)
 
     def test_test_db_page_exists(self):
         response = self.app.get("/db/test_db")
