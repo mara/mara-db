d995775f52486a8e3757fc57a093dd35237dc5f5
jacopofar <jacopofar@users.noreply.github.com>
2017-11-02 17:50:45 +0100

    Add Mypy, various small changes after code review

--- mara_db/config.py
+++ mara_db/config.py
@@ -1,9 +1,10 @@
 """Configuration of database connections"""
 
 from sqlalchemy import engine
+import typing
 
 
-def databases() -> {str: engine.Engine}:
+def databases() -> typing.Dict[str, engine.Engine]:
     """The list of database connections to use, by alias"""
     return {'mara': engine.create_engine('postgresql+psycopg2://root@localhost/mara')}
 
@@ -11,5 +12,3 @@ def databases() -> {str: engine.Engine}:
 def mara_db_alias() -> str:
     """The database alias for mara internal data"""
     return 'mara'
-
-
