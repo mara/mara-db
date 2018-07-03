47c7ecea88ee5c92fe10963b40bd1c79cf246970
Jan Katins <jan.katins@kfzteile24.de>
2018-07-03 17:06:00 +0200

    Add a postgres_connection_context context manager

--- mara_db/postgresql.py
+++ mara_db/postgresql.py
@@ -25,3 +25,24 @@ def postgres_cursor_context(alias: str) -> 'psycopg2.extensions.cursor':
     finally:
         cursor.close()
         connection.close()
+
+@contextlib.contextmanager
+def postgres_connection_context(alias: str) -> 'psycopg2.extensions.connection':
+    """Creates a context with a psycopg2 connection for a database alias"""
+    import psycopg2
+    import psycopg2.extensions
+
+    import mara_db.dbs
+
+    db = mara_db.dbs.db(alias)
+    assert (isinstance(db, mara_db.dbs.PostgreSQLDB))
+    connection = psycopg2.connect(dbname=db.database, user=db.user, password=db.password,
+                                  host=db.host, port=db.port)  # type: psycopg2.extensions.connection
+    try:
+        yield connection
+        connection.commit()
+    except Exception as e:
+        connection.rollback()
+        raise e
+    finally:
+        connection.close()
