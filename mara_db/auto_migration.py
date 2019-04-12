312ca5a3790fe5077544c37f0ac9fdd27448a5cd
Martin Loetzsch <martin.loetzsch@gmail.com>
2019-04-12 21:54:23 +0200

    move generic function for creating a sqlalchemy engine to separate file

--- mara_db/auto_migration.py
+++ mara_db/auto_migration.py
@@ -1,7 +1,6 @@
 """Auto-migration of sql alchemy models with alembic. Use with care"""
 
 import copy
-import functools
 import io
 import sys
 
@@ -12,6 +11,7 @@ from sqlalchemy import *  # unfortunately needed to get the eval part further do
 from sqlalchemy.dialects import *  # unfortunately needed to get the eval part further down working
 
 import mara_db.dbs
+from .sqlalchemy_engine import engine
 
 
 def auto_migrate(engine: sqlalchemy.engine.Engine, models: [sqlalchemy.sql.schema.MetaData]):
@@ -118,45 +118,6 @@ def auto_discover_models_and_migrate() -> bool:
                 models.append(model)
 
     return auto_migrate(engine('mara'), models)
-
-
-@functools.singledispatch
-def engine(db: object) -> sqlalchemy.engine.Engine:
-    """
-    Returns a sql alchemy engine for a configured database connection
-
-    Args:
-        db: The database to use (either an alias or a `dbs.DB` object
-
-    Returns:
-        The generated sqlalchemy engine
-
-    Examples:
-        >>> print(engine('mara'))
-        Engine(postgresql+psycopg2://None@localhost/mara)
-    """
-    pass
-
-
-@engine.register(str)
-def __(alias: str, **_):
-    return engine(mara_db.dbs.db(alias))
-
-
-@engine.register(mara_db.dbs.DB)
-def __(db, **_):
-    raise NotImplementedError(f'Please implement function engine for {db.__class__.__name__}')
-
-
-@engine.register(mara_db.dbs.PostgreSQLDB)
-def __(db: mara_db.dbs.PostgreSQLDB):
-    return sqlalchemy.create_engine(
-        f'postgresql+psycopg2://{db.user}{":"+db.password if db.password else ""}@{db.host}{":"+str(db.port) if db.port else "" }/{db.database}')
-
-
-@engine.register(mara_db.dbs.SQLiteDB)
-def __(db: mara_db.dbs.SQLiteDB):
-    return sqlalchemy.create_engine(f'sqlite:///{db.file_name}')
 
 
 if __name__ == "__main__":
---
+++ mara_db/sqlalchemy_engine.py
@@ -0,0 +1,46 @@
+import functools
+
+import sqlalchemy.engine
+import sqlalchemy.sql.schema
+
+import mara_db.dbs
+
+
+@functools.singledispatch
+def engine(db: object) -> sqlalchemy.engine.Engine:
+    """
+    Returns a sql alchemy engine for a configured database connection
+
+    Args:
+        db: The database to use (either an alias or a `dbs.DB` object
+
+    Returns:
+        The generated sqlalchemy engine
+
+    Examples:
+        >>> print(engine('mara'))
+        Engine(postgresql+psycopg2://None@localhost/mara)
+    """
+    pass
+
+
+@engine.register(str)
+def __(alias: str, **_):
+    return engine(mara_db.dbs.db(alias))
+
+
+@engine.register(mara_db.dbs.DB)
+def __(db, **_):
+    raise NotImplementedError(f'Please implement function engine for {db.__class__.__name__}')
+
+
+@engine.register(mara_db.dbs.PostgreSQLDB)
+def __(db: mara_db.dbs.PostgreSQLDB):
+    return sqlalchemy.create_engine(
+        f'postgresql+psycopg2://{db.user}{":" + db.password if db.password else ""}@{db.host}{":" + str(
+            db.port) if db.port else ""}/{db.database}')
+
+
+@engine.register(mara_db.dbs.SQLiteDB)
+def __(db: mara_db.dbs.SQLiteDB):
+    return sqlalchemy.create_engine(f'sqlite:///{db.file_name}')
