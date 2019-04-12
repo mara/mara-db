ad1a5033c326222cc42536c3060b136a175bbf01
Martin Loetzsch <martin.loetzsch@gmail.com>
2019-04-12 21:59:27 +0200

    move some imports into the functions that use them in order to improve loading speed

--- mara_db/cli.py
+++ mara_db/cli.py
@@ -4,11 +4,11 @@ import sys
 
 import click
 
-import mara_db.auto_migration
-
 
 @click.command()
 def migrate():
     """Compares the current database with all defined models and applies the diff"""
+    import mara_db.auto_migration
+
     if not mara_db.auto_migration.auto_discover_models_and_migrate():
         sys.exit(-1)
