657ce1c596e9a4145b45ebbd0b4c981c6f0ad30f
Martin Loetzsch <martin.loetzsch@gmail.com>
2019-10-16 11:46:33 +0200

    allow to pass a dbs.PostgreSQLDB instance to postgresql.postgres_cursor_context

--- mara_db/postgresql.py
+++ mara_db/postgresql.py
@@ -1,17 +1,19 @@
 """Easy access to postgres databases via psycopg2"""
 
 import contextlib
+import typing
+import mara_db.dbs
 
 
 @contextlib.contextmanager
-def postgres_cursor_context(alias: str) -> 'psycopg2.extensions.cursor':
+def postgres_cursor_context(db: typing.Union[str, mara_db.dbs.PostgreSQLDB]) -> 'psycopg2.extensions.cursor':
     """Creates a context with a psycopg2 cursor for a database alias"""
     import psycopg2
     import psycopg2.extensions
 
-    import mara_db.dbs
+    if isinstance(db, str):
+        db = mara_db.dbs.db(db)
 
-    db = mara_db.dbs.db(alias)
     assert (isinstance(db, mara_db.dbs.PostgreSQLDB))
     connection = psycopg2.connect(dbname=db.database, user=db.user, password=db.password,
                                   host=db.host, port=db.port)  # type: psycopg2.extensions.connection
