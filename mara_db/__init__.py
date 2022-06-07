2661a663cdf77b6ca98eb33b54d880690c982502
Leo Schick <leonhard.schick@gmail.com>
2022-06-07 22:39:36 +0200

    move basic config to setup.cfg, improve Makefile

--- mara_db/__init__.py
+++ mara_db/__init__.py
@@ -1,4 +1,5 @@
 """Make the functionalities of this package auto-discoverable by mara-app"""
+__version__ = '4.7.1'
 
 
 def MARA_CONFIG_MODULES():
--- setup.py
+++ setup.py
@@ -1,25 +1,6 @@
-import re
-
-from setuptools import setup, find_packages
-
-
-def get_long_description():
-    with open('README.md') as f:
-        return re.sub('!\[(.*?)\]\(docs/(.*?)\)',
-                      r'![\1](https://github.com/mara/mara-db/raw/master/docs/\2)', f.read())
-
+from setuptools import setup
 
 setup(
-    name='mara-db',
-    version='4.7.1',
-
-    description='Configuration and monitoring of database connections',
-
-    long_description=get_long_description(),
-    long_description_content_type='text/markdown',
-
-    url='https://github.com/mara/mara-db',
-
     install_requires=[
         'SQLAlchemy>=1.1.5',
         'sqlalchemy-utils>=0.32.14',
@@ -28,9 +9,6 @@ setup(
         'graphviz>=0.8',
         'mara-page>=1.3.0',
         'psycopg2-binary>=2.7.3'],
-
-    setup_requires=['setuptools_scm'],
-    include_package_data=True,
 
     extras_require={
         'test': ['pytest', 'pytest_click', 'pytest-docker', 'pytest-dependency', 'SQLAlchemy>=1.2.0'],
@@ -46,14 +24,5 @@ setup(
         'mysql': ['mysqlclient'],
         'postgres': ['psycopg2-binary>=2.7.3'],
         'redshift': ['sqlalchemy-redshift'],
-    },
-
-    packages=find_packages(),
-    package_data={'mara_db': ["*.js", "*.css", "*.css.map", "*.ico", "*.jpg", "*.png"]},
-
-    author='Mara contributors',
-    license='MIT',
-
-    entry_points={},
-    python_requires='>=3.6'
+    }
 )
