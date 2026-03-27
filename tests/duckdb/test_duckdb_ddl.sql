cfed27e42e0c85ded8cd0cedca803542f23b909e
Leo Schick <leonhard.schick@gmail.com>
2026-03-27 22:41:16 +0100

    add duckdb implementation

---
+++ tests/duckdb/test_duckdb_ddl.sql
@@ -0,0 +1,17 @@
+CREATE TABLE names
+(
+    id INT,
+    name TEXT
+);
+
+CREATE TABLE names_with_header
+(
+    id INT,
+    name TEXT
+);
+
+CREATE TABLE accounts_json
+(
+    data jsonb,
+    row INTEGER PRIMARY KEY
+);
